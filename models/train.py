014e6469c6bf004770af7ea203af38b43f9bc539
AlbertHuyb <13935047516@163.com>
2018-11-27 15:09:08 +0800

    add video_pretreat and for loop in train

--- models/train.py
+++ models/train.py
@@ -66,7 +66,6 @@ def train1step(video_net, audio_net, syn_net, image_input, spect_input, N=2):
     audio_optimizer.step()
     syn_optimizer.step()
 
-
 def test_train1step():
     N = 2
     video_net = modifyresnet18()
@@ -76,6 +75,46 @@ def test_train1step():
     spect_input = np.random.rand(N, 1, 256, 256)
     train1step(video_net, audio_net, syn_net, image_input, spect_input)
 
-
+SPEC_DIR = '/data/liyunfei/dataset/audio_spectrums'
+IMAGE_DIR = '/data/liyunfei/dataset/video_3frames'
+INSTRUMENTS = os.listdir(SPEC_DIR)
+def train_all():
+    video_net = modifyresnet18()
+    audio_net = UNet7()
+    syn_net = synthesizer()
+    for n1 in range(0,len(INSTRUMENTS)):
+        instrument1 = INSTRUMENTS[n1]
+        instrument_path1 = os.path.join(SPEC_DIR,instrument1)
+        solo_num1 = len(os.listdir(instrument_path1))
+        for n2 in range(n1,len(INSTRUMENTS)):
+            instrument2 = INSTRUMENTS[n2]
+            instrument_path2 = os.path.join(SPEC_DIR,instrument2)
+            solo_num2 = len(os.listdir(instrument_path2))
+            for s1 in range(1,solo_num1+1):
+                for s2 in range(1,solo_num2+1):
+                    solo_path1 = os.path.join(instrument_path1,str(s1))
+                    part_num1 = len(os.listdir(solo_path1))
+                    solo_path2 = os.path.join(instrument_path2,str(s2))
+                    part_num2 = len(os.listdir(solo_path2))
+                    for p1 in range(1,part_num1+1):
+                        for p2 in range(1,part_num2+1):
+                            spec1_path = os.path.join(solo_path1,str(p1)+'.npy')
+                            spec2_path = os.path.join(solo_path2,str(p2)+'.npy')
+                            video1_path = os.path.join(IMAGE_DIR,instrument1,str(s1),str(p1)+'.npy')
+                            video2_path = os.path.join(IMAGE_DIR,instrument1,str(s2),str(p2)+'.npy')
+                            spec1 = np.absolute(np.load(spec1_path))
+                            spec2 = np.absolute(np.load(spec2_path))
+                            spec1 = spec1[np.newaxis,:]
+                            spec2 = spec2[np.newaxis,:]
+                            spect_input = np.stack([spec1,spec2],axis = 0)
+                            print(spect_input.shape)
+                            video1 = np.load(video1_path)
+                            video1 = np.transpose(video1,(0,3,1,2))
+                            video2 = np.load(video2_path)
+                            video2 = np.transpose(video2,(0,3,1,2))
+                            image_input = np.stack([video1,video2],axis = 0)
+                            print(image_input.shape)
+                            #exit()
+                            train1step(video_net, audio_net, syn_net, image_input, spect_input)
 if __name__ == '__main__':
     test_train1step()
--- video_pretreat.py
+++ video_pretreat.py
@@ -9,13 +9,15 @@ import librosa.display
 import math
 import matplotlib.pyplot as plt
 import numpy as np
+import cv2
 
-NEW_DIR = './audio_spectrums'
-OLD_DIR = './audios/solo' 
+NEW_DIR = './video_3frames'
+OLD_DIR = './videos/solo' 
+BLOCK_TIME = 66302/11000
 SAMPLE_RATE = 11000
-BLOCK_LENGTH = 66302
-WINDOW_SIZE = 1022
-HOP_LENGTH = 256
+FPS = 24
+BLOCK_LENGTH = math.floor(BLOCK_TIME*FPS)
+FRAME_INDEX = [0,math.floor((BLOCK_LENGTH-1)/2),BLOCK_LENGTH-1]
 
 frequencies = np.linspace(SAMPLE_RATE/2/512,SAMPLE_RATE/2,512)
 log_freq = np.log10(frequencies)
@@ -33,34 +35,30 @@ for instrument in instrument_class:
 	files_dir = os.path.join(OLD_DIR,instrument)
 	files = os.listdir(files_dir)
 	for file in files:
-		w,sr = librosa.load(os.path.join(files_dir,file),sr=SAMPLE_RATE)
-		block_num = math.floor(len(w)/BLOCK_LENGTH)
+		cap = cv2.VideoCapture(os.path.join(files_dir,file))
+		frameCount = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+		frameWidth = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+		frameHeight = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+		block_num = math.floor(frameCount/BLOCK_LENGTH)
 		order = file[:-4]
 		destnation = os.path.join(NEW_DIR,instrument,order)
+		buf = np.empty((frameCount, frameHeight, frameWidth, 3), np.dtype('uint8'))
+		fc = 0
+		ret = True
+		while (fc < frameCount  and ret):
+			ret, buf[fc] = cap.read()
+			fc += 1
+		cap.release()
 		if os.path.exists(destnation) == False:
 			os.mkdir(destnation)
 		for i in range(block_num):
-			# select the wave data
-			data = w[i*BLOCK_LENGTH:(i+1)*BLOCK_LENGTH]
-			# STFT
-			stft_data = librosa.stft(data,n_fft = WINDOW_SIZE,hop_length = HOP_LENGTH,center = False)
-			# log scale resample
-			stft_data = stft_data[sample_index,:]
-			print(stft_data.shape)
-			# save data
-			np.save(os.path.join(destnation,str(i)),stft_data)
-			
-			# print the spectrum
-			#librosa.display.specshow(librosa.amplitude_to_db(np.abs(stft_data),ref=np.max),y_axis='log', x_axis='time')
-			#print('done!')
-			#print(np.shape(stft_data))
-			#plt.title('Power spectrogram')
-			#plt.colorbar(format='%+2.0f dB')
-			#plt.tight_layout()
-			#plt.show()
-
-			# if you want to test the code, you can add this exit() and try one example 
-			exit()
+			temp = buf[i*BLOCK_LENGTH:(i+1)*BLOCK_LENGTH,:,:,:]
+			result = temp[FRAME_INDEX,:,:,:]
+			final = np.empty((len(FRAME_INDEX),224,112,3),np.dtype('uint8'))
+			for p in range(0,len(FRAME_INDEX)):
+				final[p,:,:,:]=cv2.resize(result[p,:,:,:],(112,224))
+			np.save(os.path.join(destnation,str(i)),final)
+			#exit()
 
 			# save the wave segment
 			#librosa.output.write_wav(os.path.join(destnation,str(i)+'.wav'),data,sr) 
