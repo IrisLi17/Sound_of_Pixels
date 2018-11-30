dae1fa6f5502d2f9816793fc408c1c95cb89bbec
Iris Li <461729541@qq.com>
2018-11-30 22:19:15 +0800

    add metric

---
+++ util/metrics.py
@@ -0,0 +1,16 @@
+import mir_eval
+import numpy as np
+
+
+def compute_validation(reference_sources, estimated_sources, reference_mixed_source):
+    """
+    :param reference_sources: ndarray shape = (nsrc, nsample)
+    :param estimated_sources: ndarray shape = (nsrc, nsample)
+    :param reference_mixed_source: ndarray shape= (1, nsample)
+    :return: nsdr, sir, sar
+    """
+    nsrc = reference_sources.shape[0]
+    mixed_source = np.tile(reference_mixed_source, (nsrc, 1))  # shape = (nsrc, nsample)
+    [sdr, sir, sar, _] = mir_eval.separation.bss_eval_sources(reference_sources, estimated_sources)
+    [sdr_raw, _, _, _] = mir_eval.separation.bss_eval_sources(mixed_source, reference_sources)
+    return [sdr / sdr_raw, sir, sar]
