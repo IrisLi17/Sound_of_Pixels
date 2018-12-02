ab4e9aa147586e9f50dd1539056562c6b3fdd747
Iris Li <461729541@qq.com>
2018-12-02 15:59:51 +0800

    validate input

--- util/metrics.py
+++ util/metrics.py
@@ -11,6 +11,11 @@ def compute_validation(reference_sources, estimated_sources, reference_mixed_sou
     """
     nsrc = reference_sources.shape[0]
     mixed_source = np.tile(reference_mixed_source, (nsrc, 1))  # shape = (nsrc, nsample)
+    try:
+        mir_eval.separation.validate(reference_sources,estimated_sources)
+        mir_eval.separation.validate(mixed_source, reference_sources)
+    except:
+        return [None, None, None]
     [sdr, sir, sar, _] = mir_eval.separation.bss_eval_sources(reference_sources, estimated_sources)
     [sdr_raw, _, _, _] = mir_eval.separation.bss_eval_sources(mixed_source, reference_sources)
     return [sdr / sdr_raw, sir, sar]
