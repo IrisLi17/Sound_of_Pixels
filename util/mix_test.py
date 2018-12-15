53424eac8f5abdc620d97d9c17e85389bbeed8e4
AlbertHuyb <13935047516@163.com>
2018-12-15 16:47:00 +0800

    904 loacl change

--- util/mix_test.py
+++ util/mix_test.py
@@ -1,5 +1,8 @@
+import sys
+sys.path.append('..')
 from models.models import modifyresnet18, UNet7, synthesizer
 from util import waveoperate
+from util.datahelper import image_normalization
 
 import wave
 import os
@@ -9,18 +12,23 @@ import math
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
+import torch.nn as nn
 
 NEW_DIR = './audio_spectrums'
-SPEC_DIR = '/media/huyb/EB9C554C4A073FFC/课程用/大三上/视听导/dataset/dataset/audios/solo' 
-VIDEO_DIR = '/media/huyb/EB9C554C4A073FFC/课程用/大三上/视听导/dataset/dataset/audios/solo'
-MODEL_DIR = '../model'
+WAVE_DIR = '/media/huyb/EB9C554C4A073FFC/课程用/大三上/视听导/dataset/dataset/audios/solo'
+SPEC_DIR = '../../audio_spectrums_linear' 
+VIDEO_DIR = '../../video_3frames'
+MODEL_DIR = '../../model'
 SAMPLE_RATE = 11000
 BLOCK_LENGTH = 66303
 WINDOW_SIZE = 1022
 HOP_LENGTH = 256
 
-###############################################################################3
-# load wave data
+LOAD_WAVE = False
+LOAD_SPEC = bool(1-LOAD_WAVE)
+
+MODE = "test"
+
 frequencies = np.linspace(SAMPLE_RATE/2/512,SAMPLE_RATE/2,512)
 log_freq = np.log10(frequencies)
 sample_freq = np.linspace(log_freq[0],log_freq[-1],256)
@@ -30,134 +38,233 @@ sample_index2 = np.linspace(0,510,256)
 sample_index2 = sample_index2.astype(int)
 #print(sample_index2.dtype)
 #exit()
-
-instrument_class = os.listdir(SPEC_DIR)
-instrument1 = instrument_class[4]
-instrument2 = instrument_class[1]
-
-files_dir1 = os.path.join(SPEC_DIR,instrument1)
-files_dir2 = os.path.join(SPEC_DIR,instrument2)
-
-file_dir1 = os.listdir(files_dir1)[1]
-file_dir2 = os.listdir(files_dir2)[1]
-
 i = 1
-print("load music from",os.path.join(files_dir2,file_dir2),os.path.join(files_dir1,file_dir1))
-w,sr = librosa.load(os.path.join(files_dir1,file_dir1),sr=SAMPLE_RATE)
-data1 = w[i*BLOCK_LENGTH:(i+1)*BLOCK_LENGTH-1]
+###############################################################################3
+# load wave data
+if LOAD_WAVE:
+	instrument_class = os.listdir(WAVE_DIR)
+	instrument1 = instrument_class[4]
+	instrument2 = instrument_class[0]
 
-w,sr = librosa.load(os.path.join(files_dir2,file_dir2),sr=SAMPLE_RATE)
-data2 = w[i*BLOCK_LENGTH:(i+1)*BLOCK_LENGTH-1]
+	wave_dir1 = os.listdir(os.path.join(WAVE_DIR,instrument1))[1]
+	wave_dir2 = os.listdir(os.path.join(WAVE_DIR,instrument2))[1]
 
-data = data1 + data2
-# librosa.output.write_wav(os.path.join('./test','mix.wav'),data,sr) 
-# librosa.output.write_wav(os.path.join('./test','src1.wav'),data1,sr) 
-# librosa.output.write_wav(os.path.join('./test','src2.wav'),data2,sr) 
+	print("load music from",os.path.join(WAVE_DIR,instrument2,wave_dir2),os.path.join(WAVE_DIR,instrument1,wave_dir1))
+	w,sr = librosa.load(os.path.join(WAVE_DIR,instrument1,wave_dir1),sr=SAMPLE_RATE)
+	data1 = w[i*BLOCK_LENGTH:(i+1)*BLOCK_LENGTH-1]
 
-stft_data = librosa.stft(data,n_fft = WINDOW_SIZE,hop_length = HOP_LENGTH,center = False)
-stft_data1 = librosa.stft(data1,n_fft = WINDOW_SIZE,hop_length = HOP_LENGTH,center = False)
-stft_data2 = librosa.stft(data2,n_fft = WINDOW_SIZE,hop_length = HOP_LENGTH,center = False)
-print("length0",data1.shape)
+	w,sr = librosa.load(os.path.join(WAVE_DIR,instrument2,wave_dir2),sr=SAMPLE_RATE)
+	data2 = w[i*BLOCK_LENGTH:(i+1)*BLOCK_LENGTH-1]
 
-stft_data = stft_data[sample_index2,:]
-stft_data1 = stft_data1[sample_index2,:]
-stft_data2 = stft_data2[sample_index2,:]
+	data = data1 + data2
+	# librosa.output.write_wav(os.path.join('./test','mix.wav'),data,sr) 
+	# librosa.output.write_wav(os.path.join('./test','src1.wav'),data1,sr) 
+	# librosa.output.write_wav(os.path.join('./test','src2.wav'),data2,sr) 
 
-abs_stft_data = np.abs(stft_data)
-abs_stft_data1 = np.abs(stft_data1)
-abs_stft_data2 = np.abs(stft_data2)
+	stft_data = librosa.stft(data,n_fft = WINDOW_SIZE,hop_length = HOP_LENGTH,center = False)
+	stft_data1 = librosa.stft(data1,n_fft = WINDOW_SIZE,hop_length = HOP_LENGTH,center = False)
+	stft_data2 = librosa.stft(data2,n_fft = WINDOW_SIZE,hop_length = HOP_LENGTH,center = False)
+	print("length0",data1.shape)
+
+	stft_data = stft_data1[sample_index2,:] 
+	stft_data1 = stft_data1[sample_index2,:]
+	stft_data2 = stft_data2[sample_index2,:]
+
+	abs_stft_data1 = np.abs(stft_data1)
+	abs_stft_data2 = np.abs(stft_data2)
+	abs_stft_data = abs_stft_data1 + abs_stft_data2
+
+################################################################
+
+################################################################
+# load from spec
+if LOAD_SPEC:
+	instrument_class = os.listdir(SPEC_DIR)
+	instrument1 = instrument_class[4]
+	instrument2 = instrument_class[3]
+
+	spec_dir1 = os.listdir(os.path.join(SPEC_DIR,instrument1))[1]
+	spec_dir2 = os.listdir(os.path.join(SPEC_DIR,instrument2))[1]
+
+	stft_data1 = np.load(os.path.join(SPEC_DIR,instrument1,spec_dir1,str(i)+'.npy'))
+	stft_data2 = np.load(os.path.join(SPEC_DIR,instrument2,spec_dir2,str(i)+'.npy'))
+
+	abs_stft_data1 = librosa.amplitude_to_db(np.abs(stft_data1),ref=np.max)
+	abs_stft_data2 = librosa.amplitude_to_db(np.abs(stft_data2),ref=np.max)
+	stft_data = (stft_data1 + stft_data2)
+	abs_stft_data = abs_stft_data1 + abs_stft_data2
+
+	print("load spectrum from",os.path.join(SPEC_DIR,instrument2,spec_dir2),os.path.join(SPEC_DIR,instrument1,spec_dir1))
+
 
 ################################################################
 
 ################################################################
 # load video data
+frame_dir1 = os.listdir(os.path.join(VIDEO_DIR,instrument1))[0]
+frame_dir2 = os.listdir(os.path.join(VIDEO_DIR,instrument2))[0]
 
-mask = abs_stft_data1 >= abs_stft_data2
+frame_data1 = np.load(os.path.join(VIDEO_DIR,instrument1,frame_dir1,str(i)+'.npy'))
+frame_data2 = np.load(os.path.join(VIDEO_DIR,instrument2,frame_dir2,str(i)+'.npy'))
+print("load frame from",os.path.join(VIDEO_DIR,instrument2,frame_dir2),os.path.join(VIDEO_DIR,instrument1,frame_dir1))
 
-video_net = modifyresnet18()
-audio_net = UNet7()
-syn_net = synthesizer()
+frame_data1 = np.transpose(frame_data1,(0,3,1,2))
+frame_data2 = np.transpose(frame_data2,(0,3,1,2))
+#################################################################
+
+#################################################################
+# prepare for network
+abs_stft_data = abs_stft_data[np.newaxis,np.newaxis,:]
+frame_data1 = frame_data1[np.newaxis,:]
+frame_data2 = frame_data2[np.newaxis,:]
+#################################################################
+
+# mask = abs_stft_data1 >= abs_stft_data2
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+video_net = modifyresnet18(1).to(device)
+audio_net = UNet7().to(device)
+syn_net = synthesizer().to(device)
 
 video_net.load_state_dict(torch.load(os.path.join(MODEL_DIR, 'video_net_params.pkl')))
 audio_net.load_state_dict(torch.load(os.path.join(MODEL_DIR, 'audio_net_params.pkl')))
 syn_net.load_state_dict(torch.load(os.path.join(MODEL_DIR, 'syn_net_params.pkl')))
 
-print(mask)
+audio_input = torch.from_numpy(abs_stft_data).to(device)
+audio_out = audio_net.forward(audio_input)
 
-result1 = np.zeros(stft_data.shape,dtype = complex)
-result2 = np.zeros(stft_data.shape,dtype = complex)
-result1[mask] = stft_data[mask]
-result2[~mask] = stft_data[~mask]
+if MODE != "test":	
+	video_input = image_normalization(frame_data1).to(device)	
+	video_out = video_net.forward(video_input[0,:,:,:,:])
+	temp = video_out * audio_out
+	temp = torch.transpose(temp, 1, 2)
+	temp = torch.transpose(temp, 2, 3)
+	syn_act = syn_net.forward(temp)
+	syn_act = torch.transpose(syn_act, 2, 3)
+	syn_act = torch.transpose(syn_act, 1, 2)
 
-'''
-plt.subplot(1,3,1)
-librosa.display.specshow((np.abs(stft_data)),y_axis='linear', x_axis='time')
-plt.subplot(1,3,2)
-librosa.display.specshow((np.abs(result1)),y_axis='linear', x_axis='time')
-plt.subplot(1,3,3)
-librosa.display.specshow((np.abs(result2)),y_axis='linear', x_axis='time')
-plt.show()
-'''
+	mask = syn_act[0,0,:,:].cpu().detach().numpy()
+	print(mask[0:10,0])
 
-# result_data1 = librosa.istft(result1,hop_length = HOP_LENGTH,win_length=1022)
-# result_data2 = librosa.istft(result2,hop_length = HOP_LENGTH,win_length=1022)
+	result1 = np.zeros(stft_data.shape,dtype = complex)
+	result1 = stft_data * mask
 
-result_data1 = waveoperate.mask2wave(result1,'linear')
-result_data2 = waveoperate.mask2wave(result2,'linear')
+	video_input = image_normalization(frame_data2).to(device)
+	video_out = video_net.forward(video_input[0,:,:,:,:])
+	print(video_out.shape)
+	temp = video_out * audio_out
+	temp = torch.transpose(temp, 1, 2)
+	temp = torch.transpose(temp, 2, 3)
+	syn_act = syn_net.forward(temp)
+	syn_act = torch.transpose(syn_act, 2, 3)
+	syn_act = torch.transpose(syn_act, 1, 2)
 
-librosa.output.write_wav(os.path.join('./test','result1.wav'),result_data1,sr) 
-librosa.output.write_wav(os.path.join('./test','result2.wav'),result_data2,sr) 
+	mask = syn_act[0,0,:,:].cpu().detach().numpy()
+	print(mask[0:10,0])
+
+	result2 = np.zeros(stft_data.shape,dtype = complex)
+	result2 = stft_data * mask
+
+	plt.subplot(1,3,1)
+	librosa.display.specshow((np.abs(stft_data)),y_axis='linear', x_axis='time')
+	plt.subplot(1,3,2)
+	librosa.display.specshow((np.abs(result1)),y_axis='linear', x_axis='time')
+	plt.subplot(1,3,3)
+	librosa.display.specshow((np.abs(result2)),y_axis='linear', x_axis='time')
+	plt.show()
 
 
+	# result_data1 = librosa.istft(result1,hop_length = HOP_LENGTH,win_length=1022)
+	# result_data2 = librosa.istft(result2,hop_length = HOP_LENGTH,win_length=1022)
 
-'''
-for instrument in instrument_class:
-	print(instrument)
-	if os.path.exists(os.path.join(NEW_DIR,instrument)) == False:
-		os.mkdir(os.path.join(NEW_DIR,instrument))
-	files_dir = os.path.join(OLD_DIR,instrument)
-	files = os.listdir(files_dir)
-	for file in files:
-		w,sr = librosa.load(os.path.join(files_dir,file),sr=SAMPLE_RATE)
-		block_num = math.floor(len(w)/BLOCK_LENGTH)
-		order = file[:-4]
-		destnation = os.path.join(NEW_DIR,instrument,order)
-		if os.path.exists(destnation) == False:
-			os.mkdir(destnation)
-		num = 0
-		for i in range(block_num):
-			num += 1
-			# select the wave data
-			data = w[i*BLOCK_LENGTH:(i+1)*BLOCK_LENGTH-1]
-			# STFT
-			librosa.output.write_wav('raw.wav',data,sr) 
-			stft_data = librosa.stft(data,n_fft = WINDOW_SIZE,hop_length = HOP_LENGTH,center = False)
-			wave = librosa.istft(stft_data,hop_length = HOP_LENGTH)
-			librosa.output.write_wav('new.wav',wave,sr) 
-			wave = librosa.istft(np.abs(stft_data),hop_length = HOP_LENGTH)
-			librosa.output.write_wav('abs.wav',wave,sr) 
-			# log scale resample
-			#print(sample_index)
-			#stft_data = stft_data[sample_index,:]
-			print(os.path.join(destnation,str(i)+'.wav'))
-			exit()
-			print(sample_index2)
-			stft_data = stft_data[tuple(sample_index2),:]
-			# save data
-			np.save(os.path.join(destnation,str(i)),stft_data)
-			
-			# print the spectrum
-			librosa.display.specshow((np.abs(stft_data)),y_axis='log', x_axis='time')
-			print('done!')
-			#print(np.shape(stft_data))
-			plt.title('Power spectrogram')
-			plt.colorbar(format='%+2.0f dB')
-			plt.tight_layout()
-			plt.show()
+	mix_wave = waveoperate.mask2wave(stft_data,'linear')
+	result_data1 = waveoperate.mask2wave(result1,'linear')
+	result_data2 = waveoperate.mask2wave(result2,'linear')
 
-			# if you want to test the code, you can add this exit() and try one example 
-			exit()
+	librosa.output.write_wav(os.path.join('./test','mix.wav'),result_data1,SAMPLE_RATE) 
+	librosa.output.write_wav(os.path.join('./test','result1.wav'),result_data1,SAMPLE_RATE) 
+	librosa.output.write_wav(os.path.join('./test','result2.wav'),result_data2,SAMPLE_RATE) 
+else:
+	video_input = image_normalization(frame_data1).to(device)
+	video_out = video_net.forward(video_input[0,:,:,:,:],mode=MODE)
+	layer = nn.MaxPool2d(kernel_size = 14)
+	# video_out = layer(video_out)
+	column = 1
+	row = 1
+	column_init = 0
+	row_init = 0
 
-			# save the wave segment
-			librosa.output.write_wav(os.path.join(destnation,str(i)+'.wav'),data,sr) 
-'''
+	# for kk in range(0,16):
+	# 	print(np.mean(video_out[0,kk,:,:].cpu().detach().numpy()))
+
+	for m in range(row_init,row_init+row):
+		for n in range(column_init,column_init+column):
+			video_select = video_out[:,:,m,n]
+			video_select = video_select[:,np.newaxis,np.newaxis]
+			video_select = torch.transpose(video_select,0,2)
+			video_select = torch.transpose(video_select,1,3)
+			print(video_select.shape,audio_out.shape)
+			temp = video_select * audio_out
+			temp = torch.transpose(temp, 1, 2)
+			temp = torch.transpose(temp, 2, 3)
+			syn_act = syn_net.forward(temp)
+			syn_act = torch.transpose(syn_act, 2, 3)
+			syn_act = torch.transpose(syn_act, 1, 2)
+
+			mask = syn_act[0,0,:,:].cpu().detach().numpy()
+			print(mask[0:10,0])
+
+			stft_data = stft_data1 + stft_data2
+			result = stft_data * mask
+			result_data = waveoperate.mask2wave(result,'linear')
+			librosa.output.write_wav(os.path.join('./test','result1.wav'),result_data,SAMPLE_RATE) 
+
+			# plt.subplot(row,column,(m-row_init)*row+(n-column_init)+1)
+			plt.subplot(row,column,0+(m-row_init)*row+(n-column_init)+1)
+			result = librosa.amplitude_to_db(np.abs(result),ref = np.max)
+			librosa.display.specshow(result,y_axis='linear', x_axis='time')
+
+	plt.show()
+
+
+	video_input = image_normalization(frame_data2).to(device)
+	video_out = video_net.forward(video_input[0,:,:,:,:],mode=MODE)
+	layer = nn.MaxPool2d(kernel_size = 14)
+	# video_out = layer(video_out)
+
+	# for kk in range(0,16):
+	# 	print(np.mean(video_out[0,kk,:,:].cpu().detach().numpy()))
+
+	plt.figure()
+	for m in range(row_init,row_init+row):
+		for n in range(column_init,column_init+column):
+			video_select = video_out[:,:,m,n]
+			video_select = video_select[:,np.newaxis,np.newaxis]
+			video_select = torch.transpose(video_select,0,2)
+			video_select = torch.transpose(video_select,1,3)
+			print(video_select.shape,audio_out.shape)
+			temp = video_select * audio_out
+			temp = torch.transpose(temp, 1, 2)
+			temp = torch.transpose(temp, 2, 3)
+			syn_act = syn_net.forward(temp)
+			syn_act = torch.transpose(syn_act, 2, 3)
+			syn_act = torch.transpose(syn_act, 1, 2)
+
+			mask = syn_act[0,0,:,:].cpu().detach().numpy()
+			print(mask[0:10,0])
+
+			stft_data = stft_data1 + stft_data2
+			result = stft_data * mask
+
+			result_data = waveoperate.mask2wave(result,'linear')
+			librosa.output.write_wav(os.path.join('./test','result2.wav'),result_data,SAMPLE_RATE)
+
+			# plt.subplot(row,column,(m-row_init)*row+(n-column_init)+1)
+			plt.subplot(row,column,(m-row_init)*row+(n-column_init)+1)
+			result = librosa.amplitude_to_db(np.abs(result),ref = np.max)
+			librosa.display.specshow(result,y_axis='linear', x_axis='time')
+
+	plt.show()
+
+
